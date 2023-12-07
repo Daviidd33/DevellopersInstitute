b1c551bef97a626d1bac8cd38dd0a0acda260d9b
David <dav7@hotmail.fr>
2023-12-07 09:47:30 +0200

    dailyChallenge W2 d3

---
+++ week2/Day3/DailyChallenge/script.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program that recreates the pattern below.
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * * * *
+
+// const number = 6
+// let stars = " "
+
+// for (let i = 0; i < number; i++) {
+//     stars = stars + " * ";
+//     console.log(stars);
+// }
