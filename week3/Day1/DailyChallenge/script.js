71724881156daf05a596a08898dd8f4fa403f8fb
David <dav7@hotmail.fr>
2024-01-09 15:12:55 +0200

    daily d1 w3

---
+++ week3/Day1/DailyChallenge/script.js
@@ -0,0 +1,25 @@
+
+// // 1.Create an array which value is the planets of the solar system.
+// const planets = ["Venus", "Pluton", "Terre", "Saturne", "Mercury"]
+// const section = document.querySelector(".listPlanets")
+
+// // 2.For each planet in the array, create a < div > using createElement.
+// // This div should have a class named "planet".
+
+// for (const planet of planets) {
+
+//     let div = document.createElement("div");
+//     div.classList.add("planet")
+//     div.classList.add(planet.toLocaleLowerCase())
+//     section.appendChild(div)
+//     // console.log(div);
+
+// }
+
+// // 3.Each planet should have a different background color.
+// // (Hint: you could add a new class to each planet - each class containing a different background - color).
+// // Finally append each div to the < section > in the HTML(presented below).
+
+
+
+
